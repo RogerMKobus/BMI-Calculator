17c120bc62788545d6a5bccf367c83bf141a8036
RogerMKobus <roger.kobus@gmail.com>
2021-03-17 17:08:25 -0300

    Calculate function

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        calculateButton?.setOnClickListener{
+            calculate(weightEditText.text.toString(), heighEditText.text.toString())
+        }
+    }
+
+    private fun calculate(weight: String, height: String){
+        val weight = weight.toFloatOrNull()
+        val height = height.toFloatOrNull()
+
+        if ( weight != null && height != null){
+            val bmi = weight / (height * height)
+            print(bmi.roundToInt())
+            titleBMI.text = ("Your BMI is \n" +
+                    "%.2f").format(bmi)
+        }
     }
 }
