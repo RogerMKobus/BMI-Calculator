3848e8d04a464ebb3c260e7b88c02a2be3c0f1c8
RogerMKobus <roger.kobus@gmail.com>
2021-03-18 17:18:44 -0300

    Imperial System calculation added

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bmicalculator
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,45 +15,107 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     private fun setListeners() {
         calculateButton?.setOnClickListener{
-            calculate(weightEditText.text.toString(), heighEditText.text.toString())
+            calculate()
+        }
+
+        val switch = findViewById<Switch>(R.id.switchMetrics)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                switchMetrics.text = "Imperial System"
+                weightEditText.hint = "Insert your weight in pounds"
+                heighEditText.hint = "Insert you height in feet"
+                inchesEditText.visibility = View.VISIBLE
+            } else {
+                switchMetrics.text = "Metrics System"
+                weightEditText.hint = "Insert your weight in kg"
+                heighEditText.hint = "Insert you height in cm"
+                inchesEditText.visibility = View.GONE
+            }
         }
     }
 
-    private fun calculate(weight: String, height: String){
-        val weight = weight.toFloatOrNull()
-        val height = height.toFloatOrNull()
+    private fun calculate(){
 
-        if ( weight != null && height != null){
-            val bmi = weight / (height * height)
-            val result: String?
+        var check = switchMetrics.isChecked
 
-            result = when {
-                bmi <= 18.5 -> {
-                    "Underweight"
+        if (check == true) {
+            val pounds = weightEditText.text.toString().toFloatOrNull()
+            val feet = heighEditText.text.toString().toFloatOrNull()
+            val inches = inchesEditText.text.toString().toFloatOrNull()
+
+            if ( pounds != null && feet != null && inches != null){
+                // First you get the inches by converting the feet to inches and adding the rest wich is our inches val
+                // then you divide the pounds for the total of inches 2 times, and multiply by 703 and you'll get the answer
+
+                val bmi = ((pounds / (feet * 12 + inches)) / (feet * 12 + inches)) * 703
+                val result: String?
+
+                result = when {
+                    bmi <= 18.5 -> {
+                        "Underweight"
+                    }
+                    bmi in 18.5..24.9 -> {
+                        "Normal range"
+                    }
+                    bmi in 25.0..29.9 -> {
+                        "Overweight"
+                    }
+                    bmi in 30.0..34.9 -> {
+                        "Obese class I"
+                    }
+                    bmi in 35.0..39.9 -> {
+                        "Obese class II"
+                    }
+                    else -> {
+                        "Obese class III"
+                    }
                 }
-                bmi in 18.5..24.9 -> {
-                    "Normal range"
-                }
-                bmi in 25.0..29.9 -> {
-                    "Overweight"
-                }
-                bmi in 30.0..34.9 -> {
-                    "Obese class I"
-                }
-                bmi in 35.0..39.9 -> {
-                    "Obese class II"
-                }
-                else -> {
-                    "Obese class III"
-                }
+
+                val intent = Intent(this@MainActivity,ResultActivity::class.java)
+                intent.putExtra("bmi", String.format("%.2f", bmi))
+                intent.putExtra("result", result)
+                startActivity(intent)
             }
+        }
 
-            val intent = Intent(this@MainActivity,ResultActivity::class.java)
-            intent.putExtra("bmi", String.format("%.2f", bmi))
-            intent.putExtra("result", result)
-            startActivity(intent)
+        else {
+            val kg = weightEditText.text.toString().toFloatOrNull()
+            val cm = heighEditText.text.toString().toFloatOrNull()
+
+            if ( kg != null && cm != null){
+                val bmi = kg / (cm * cm)
+                val result: String?
+
+                result = when {
+                    bmi <= 18.5 -> {
+                        "Underweight"
+                    }
+                    bmi in 18.5..24.9 -> {
+                        "Normal range"
+                    }
+                    bmi in 25.0..29.9 -> {
+                        "Overweight"
+                    }
+                    bmi in 30.0..34.9 -> {
+                        "Obese class I"
+                    }
+                    bmi in 35.0..39.9 -> {
+                        "Obese class II"
+                    }
+                    else -> {
+                        "Obese class III"
+                    }
+                }
+
+                val intent = Intent(this@MainActivity,ResultActivity::class.java)
+                intent.putExtra("bmi", String.format("%.2f", bmi))
+                intent.putExtra("result", result)
+                startActivity(intent)
+        }
         }
     }
 }
