cc4336be3d5f676f7fca2efc69f1f39891bb170c
RogerMKobus <roger.kobus@gmail.com>
2021-03-18 21:08:31 -0300

    Added background color to result

--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -7,13 +7,38 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         resultText.text = "BMI: "+ intent.getStringExtra("bmi") +
                 "\n\nWeight status: " + intent.getStringExtra("result")
+
+        val bmi = intent.getStringExtra("bmi")?.toFloat()
+
+        if (bmi != null) {
+            when {
+                bmi <= 18.5 -> {
+                    resultText.setBackgroundResource(R.color.underweight)
+                }
+                bmi in 18.5..24.9 -> {
+                    resultText.setBackgroundResource(R.color.normal)
+                }
+                bmi in 25.0..29.9 -> {
+                    resultText.setBackgroundResource(R.color.overweight)
+                }
+                bmi in 30.0..34.9 -> {
+                    resultText.setBackgroundResource(R.color.obeseI)
+                }
+                bmi in 35.0..39.9 -> {
+                    resultText.setBackgroundResource(R.color.obeseII)
+                }
+                else -> {
+                    resultText.setBackgroundResource(R.color.obeseIII)
+                }
+            }
+        }
 
         setListeners()
     }
